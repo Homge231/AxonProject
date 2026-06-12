30db82e78a9f272a57d8d873c0f6f7da08fe4dfe
Bao Nguyen Hoang Gia <nhgbao@dn-minimac47.aavn.local>
2026-06-12 09:58:09 +0700

    init: phaser + vue hybrid setup

---
+++ client/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="relative w-screen h-screen overflow-hidden bg-background">
+    <div id="phaser-container" class="absolute inset-0 z-0" />
+    <div class="absolute inset-0 z-10">
+      <RouterView />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { onMounted, onUnmounted } from 'vue'
+import Phaser from 'phaser'
+import { createPhaserGame } from './game/PhaserGame'
+
+let game: Phaser.Game | null = null
+
+onMounted(() => {
+  game = createPhaserGame('phaser-container')
+})
+
+onUnmounted(() => {
+  game?.destroy(true)
+})
+</script>
---
+++ client/tailwind.config.js
@@ -0,0 +1,23 @@
+export default {
+  darkMode: 'class',
+  content: ['./index.html', './src/**/*.{vue,ts}'],
+  theme: {
+    extend: {
+      colors: {
+        'primary-fixed': '#7df4ff',
+        'background': '#10131a',
+        'surface-container': '#1d2026',
+        'surface-container-high': '#272a31',
+        'surface-container-highest': '#32353c',
+        'on-surface': '#e1e2eb',
+        'on-surface-variant': '#b9cacb',
+        'secondary': '#ebb2ff',
+        'secondary-fixed': '#f8d8ff',
+        'tertiary': '#dcffe2',
+        'tertiary-fixed': '#5bffa1',
+        'outline-variant': '#3b494b',
+        'error': '#ffb4ab',
+      }
+    }
+  }
+}
