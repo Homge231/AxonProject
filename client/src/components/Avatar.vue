e321cd4ac2212b7d06d8822f388c408c7ea5c645
Homge231 <homge231@gmail.com>
2026-06-30 09:50:46 +0700

    feature: Core Scoring Engine & API [AI 0%]

--- client/src/components/Avatar.vue
+++ client/src/components/Avatar.vue
@@ -1,7 +1,7 @@
 <template>
   <div
     v-if="src"
-    class="fixed top-24 left-8 lg:left-12 z-20 pointer-events-none"
+    class="fixed bottom-8 left-8 lg:left-12 z-20 pointer-events-none"
   >
     <div class="w-14 h-14 rounded-full bg-gradient-to-br from-blue to-lightBlue p-0.5 shadow-[0_0_15px_rgba(59,130,246,0.3)]">
       <img
